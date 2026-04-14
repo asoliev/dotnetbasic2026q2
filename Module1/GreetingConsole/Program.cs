4512d2626618c6d5f7bc6ce99f462bb8713b9668
asoliev <91322310+asoliev@users.noreply.github.com>
2026-04-14 19:23:14 +0500

    task1-GreetingConsole

---
+++ Module1/GreetingConsole/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+if (args.Length == 0)
+{
+    Console.WriteLine("Please provide a username as a command line argument.");
+    return;
+}
+
+string username = args[0];
+Console.WriteLine($"Hello, {username}");
