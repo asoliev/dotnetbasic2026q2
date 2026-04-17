a13ff3209d1aa12a8513f5dcc1c6e31840ecc8a4
asoliev <91322310+asoliev@users.noreply.github.com>
2026-04-18 04:34:41 +0500

    Fix Sort and IndexOf logic; add null checks
    Improved Sort by fixing comparison and swap logic. Updated IndexOf to use correct indexing and range. Added ArgumentNullException checks for input validation in both methods.

--- Moudle2_DebuggingFundamentals/Task1/Utilities.cs
+++ Moudle2_DebuggingFundamentals/Task1/Utilities.cs
@@ -10,16 +10,17 @@ namespace Task1
         /// <param name="numbers">Numbers to sort.</param>
         public static void Sort(int[] numbers)
         {
+            ArgumentNullException.ThrowIfNull(numbers);
             int temp;
             for (int i = 0; i < numbers.Length; i++)
             {
                 for (int j = i; j < numbers.Length; j++)
                 {
-                    if (numbers[i] < numbers[j])
+                    if (numbers[i] > numbers[j])
                     {
                         temp = numbers[i];
-                        numbers[i] = temp;
-                        numbers[j] = numbers[i];
+                        numbers[i] = numbers[j];
+                        numbers[j] = temp;
                     }
                 }
             }
@@ -35,12 +36,14 @@ namespace Task1
         /// otherwise -1.</returns>
         public static int IndexOf(Product[] products, Predicate<Product> predicate)
         {
-            for (int i = 0; i < products.Length - 1; i++)
+            ArgumentNullException.ThrowIfNull(products);
+            ArgumentNullException.ThrowIfNull(predicate);
+            for (int i = 0; i < products.Length; i++)
             {
-                var product = products[i - 1];
+                var product = products[i];
                 if (predicate(product))
                 {
-                    return --i;
+                    return i;
                 }
             }
 
