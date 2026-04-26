fd150ec8ca48039d2078bd7817fd4ce23fb2eeb3
asoliev <91322310+asoliev@users.noreply.github.com>
2026-04-26 18:00:43 +0500

    Refactor codebase; update tests and dependencies
    - Updated test dependencies to latest NUnit and SDK versions
    - Refactored tests to use C# 12 collection expressions and modern assertions
    - Simplified Product class with primary constructor and improved equality
    - Streamlined Utilities class with file-scoped namespace and clearer methods

--- Moudle2_DebuggingFundamentals/Task1.Tests/UtilitiesTests.cs
+++ Moudle2_DebuggingFundamentals/Task1.Tests/UtilitiesTests.cs
@@ -1,131 +1,129 @@
 using NUnit.Framework;
 using System;
-using Task1;
 
-namespace Task1.Tests
+namespace Task1.Tests;
+
+[TestFixture]
+public class Tests
 {
-    [TestFixture]
-    public class Tests
+    [Test]
+    public void Sort_Numbers_ReturnsAscendingSortedNumbers()
     {
-        [Test]
-        public void Sort_Numbers_ReturnsAscendingSortedNumbers()
+        int[] numbers = [4, 2, 1, 3, -5];
+
+        Utilities.Sort(numbers);
+
+        Assert.That( numbers, Is.EqualTo([-5, 1, 2, 3, 4]) );
+    }
+
+    [Test]
+    public void Sort_Null_ThrowsArgumentNullException()
+    {
+        Assert.That(() => Utilities.Sort(null), Throws.InstanceOf<ArgumentNullException>());
+    }
+
+    [Test]
+    public void Sort_EmptyArray_ReturnsEmptyArray()
+    {
+        int[] numbers = [];
+
+        Utilities.Sort(numbers);
+
+        Assert.That( numbers, Is.EqualTo( Array.Empty<int>() ) );
+    }
+
+    [Test]
+    public void IndexOf_Products_ReturnsTwo()
+    {
+        Product[] products =
+        [
+            new("Product 1", 10.0d),
+            new("Product 2", 20.0d),
+            new("Product 3", 30.0d),
+        ];
+        var productToFind = new Product("Product 3", 30.0d);
+
+        int index = Utilities.IndexOf(products, product => product.Equals(productToFind));
+
+        Assert.That(index, Is.EqualTo(2));
+    }
+
+    [Test]
+    public void IndexOf_NoMatch_ReturnsMinusOne()
+    {
+        Product[] products =
+        [
+            new("Product 1", 10.0d),
+            new("Product 2", 20.0d),
+            new("Product 3", 30.0d),
+        ];
+        var productToFind = new Product("Product 4", 30.0d);
+
+        int index = Utilities.IndexOf(products, product => product.Equals(productToFind));
+
+        Assert.That(index, Is.EqualTo(-1));
+    }
+
+    [Test]
+    public void IndexOf_EqualsWithNull_ReturnsMinusOne()
+    {
+        Product[] products =
+        [
+            new("Product 1", 10.0d),
+            new("Product 2", 20.0d),
+            new("Product 3", 30.0d),
+        ];
+        Product productToFind = null;
+
+        int index = Utilities.IndexOf(products, product => product.Equals(productToFind));
+
+        Assert.That(index, Is.EqualTo(-1));
+    }
+
+    [Test]
+    public void IndexOf_SearchForNonProductTypeObject_ReturnsMinusOne()
+    {
+        Product[] products =
+        [
+            new("Product 1", 10.0d),
+            new("Product 2", 20.0d),
+            new("Product 3", 30.0d),
+        ];
+        int productToFind = 42;
+
+        int index = Utilities.IndexOf(products, product => product.Equals(productToFind));
+
+        Assert.That(index, Is.EqualTo(-1));
+    }
+
+    [Test]
+    public void IndexOf_NullProducts_ThrowsArgumentNullException()
+    {
+        Assert.That(() =>
         {
-            int[] numbers = new[] { 4, 2, 1, 3, -5 };
-
-            Utilities.Sort(numbers);
-
-            CollectionAssert.AreEqual(new[] { -5, 1, 2, 3, 4 }, numbers);
-        }
-
-        [Test]
-        public void Sort_Null_ThrowsArgumentNullException()
-        {
-            Assert.That(() => Utilities.Sort(null), Throws.InstanceOf<ArgumentNullException>());
-        }
-
-        [Test]
-        public void Sort_EmptyArray_ReturnsEmptyArray()
-        {
-            int[] numbers = new int[0];
-
-            Utilities.Sort(numbers);
-
-            CollectionAssert.AreEqual(new int[0], numbers);
-        }
-
-        [Test]
-        public void IndexOf_Products_ReturnsTwo()
-        {
-            var products = new Product[]
-            {
-                new Product("Product 1", 10.0d),
-                new Product("Product 2", 20.0d),
-                new Product("Product 3", 30.0d),
-            };
             var productToFind = new Product("Product 3", 30.0d);
+            int index = Utilities.IndexOf(null, product => product.Equals(productToFind));
+        }, Throws.InstanceOf<ArgumentNullException>());
+    }
 
-            int index = Utilities.IndexOf(products, product => product.Equals(productToFind));
-
-            Assert.That(index, Is.EqualTo(2));
-        }
-
-        [Test]
-        public void IndexOf_NoMatch_ReturnsMinusOne()
+    [Test]
+    public void IndexOf_NullPredicate_ThrowsArgumentNullException()
+    {
+        Assert.That(() =>
         {
-            var products = new Product[]
-            {
-                new Product("Product 1", 10.0d),
-                new Product("Product 2", 20.0d),
-                new Product("Product 3", 30.0d),
-            };
-            var productToFind = new Product("Product 4", 30.0d);
+            Product[] products = [ new("Product 1", 10.0d) ];
+            int index = Utilities.IndexOf(products, null);
+        }, Throws.InstanceOf<ArgumentNullException>());
+    }
 
-            int index = Utilities.IndexOf(products, product => product.Equals(productToFind));
+    [Test]
+    public void IndexOf_EmptyArray_ReturnsMinusOne()
+    {
+        Product[] products = [];
+        var productToFind = new Product("Product 3", 30.0d);
 
-            Assert.That(index, Is.EqualTo(-1));
-        }
+        int index = Utilities.IndexOf(products, product => product.Equals(productToFind));
 
-        [Test]
-        public void IndexOf_EqualsWithNull_ReturnsMinusOne()
-        {
-            var products = new Product[]
-            {
-                new Product("Product 1", 10.0d),
-                new Product("Product 2", 20.0d),
-                new Product("Product 3", 30.0d),
-            };
-            Product productToFind = null;
-
-            int index = Utilities.IndexOf(products, product => product.Equals(productToFind));
-
-            Assert.That(index, Is.EqualTo(-1));
-        }
-
-        [Test]
-        public void IndexOf_SearchForNonProductTypeObject_ReturnsMinusOne()
-        {
-            var products = new Product[]
-            {
-                new Product("Product 1", 10.0d),
-                new Product("Product 2", 20.0d),
-                new Product("Product 3", 30.0d),
-            };
-            var productToFind = 42;
-
-            int index = Utilities.IndexOf(products, product => product.Equals(productToFind));
-
-            Assert.That(index, Is.EqualTo(-1));
-        }
-
-        [Test]
-        public void IndexOf_NullProducts_ThrowsArgumentNullException()
-        {
-            Assert.That(() =>
-            {
-                var productToFind = new Product("Product 3", 30.0d);
-                int index = Utilities.IndexOf(null, product => product.Equals(productToFind));
-            }, Throws.InstanceOf<ArgumentNullException>());
-        }
-
-        [Test]
-        public void IndexOf_NullPredicate_ThrowsArgumentNullException()
-        {
-            Assert.That(() =>
-            {
-                var products = new Product[] { new Product("Product 1", 10.0d) };
-                int index = Utilities.IndexOf(products, null);
-            }, Throws.InstanceOf<ArgumentNullException>());
-        }
-
-        [Test]
-        public void IndexOf_EmptyArray_ReturnsMinusOne()
-        {
-            var products = new Product[0];
-            var productToFind = new Product("Product 3", 30.0d);
-
-            int index = Utilities.IndexOf(products, product => product.Equals(productToFind));
-
-            Assert.That(index, Is.EqualTo(-1));
-        }
+        Assert.That(index, Is.EqualTo(-1));
     }
 }
--- Moudle2_DebuggingFundamentals/Task1/Utilities.cs
+++ Moudle2_DebuggingFundamentals/Task1/Utilities.cs
@@ -1,57 +1,45 @@
 ﻿using System;
 
-namespace Task1
+namespace Task1;
+
+public static class Utilities
 {
-    public static class Utilities
+    /// <summary>
+    /// Sorts an array in ascending order using bubble sort.
+    /// </summary>
+    /// <param name="numbers">Numbers to sort.</param>
+    public static void Sort(int[] numbers)
     {
-        /// <summary>
-        /// Sorts an array in ascending order using bubble sort.
-        /// </summary>
-        /// <param name="numbers">Numbers to sort.</param>
-        public static void Sort(int[] numbers)
-        {
-            ArgumentNullException.ThrowIfNull(numbers);
-            int temp;
-            for (int i = 0; i < numbers.Length; i++)
-            {
-                for (int j = i; j < numbers.Length; j++)
+        ArgumentNullException.ThrowIfNull(numbers);
+        int temp;
+        for (int i = 0; i < numbers.Length; i++)
+            for (int j = i; j < numbers.Length; j++)
+                if (numbers[i] > numbers[j])
                 {
-                    if (numbers[i] > numbers[j])
-                    {
-                        temp = numbers[i];
-                        numbers[i] = numbers[j];
-                        numbers[j] = temp;
-                    }
+                    temp = numbers[i];
+                    numbers[i] = numbers[j];
+                    numbers[j] = temp;
                 }
-            }
-        }
+    }
 
-        /// <summary>
-        /// Searches for the index of a product in an <paramref name="products"/> 
-        /// based on a <paramref name="predicate"/>.
-        /// </summary>
-        /// <param name="products">Products used for searching.</param>
-        /// <param name="predicate">Product predicate.</param>
-        /// <returns>If match found then returns index of product in <paramref name="products"/>
-        /// otherwise -1.</returns>
-        public static int IndexOf(Product[] products, Predicate<Product> predicate)
-        {
-            ArgumentNullException.ThrowIfNull(products);
-            ArgumentNullException.ThrowIfNull(predicate);
+    /// <summary>
+    /// Searches for the index of a product in an <paramref name="products"/> 
+    /// based on a <paramref name="predicate"/>.
+    /// </summary>
+    /// <param name="products">Products used for searching.</param>
+    /// <param name="predicate">Product predicate.</param>
+    /// <returns>If match found then returns index of product in <paramref name="products"/>
+    /// otherwise -1.</returns>
+    public static int IndexOf(Product[] products, Predicate<Product> predicate)
+    {
+        ArgumentNullException.ThrowIfNull(products);
+        ArgumentNullException.ThrowIfNull(predicate);
 
-            if (products.Length > 0)
-            {
-                for (int i = 0; i < products.Length; i++)
-                {
-                    var product = products[i];
-                    if (predicate(product))
-                    {
-                        return i;
-                    }
-                }
-            }
+        if (products.Length > 0)
+            for (int i = 0; i < products.Length; i++)
+                if (predicate(products[i]))
+                    return i;
 
-            return -1;
-        }
+        return -1;
     }
 }
