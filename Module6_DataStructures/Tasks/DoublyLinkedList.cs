aa5fd99b51a779aa6a5fc69d0ecacb6349a7dcf6
asoliev <91322310+asoliev@users.noreply.github.com>
2026-06-11 21:49:17 +0500

    Implement DoublyLinkedList and HybridFlowProcessor with basic operations; update .gitignore and project file

--- Module6_DataStructures/Tasks/DoublyLinkedList.cs
+++ Module6_DataStructures/Tasks/DoublyLinkedList.cs
@@ -1,47 +1,191 @@
-﻿using System;
+﻿#nullable enable
+
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Tasks.DoNotChange;
 
-namespace Tasks
+namespace Tasks;
+
+public class DoublyLinkedList<T> : IDoublyLinkedList<T>
 {
-    public class DoublyLinkedList<T> : IDoublyLinkedList<T>
+    private sealed class Node(T value)
     {
-        public int Length => throw new NotImplementedException();
+        public T Value { get; } = value;
 
-        public void Add(T e)
+        public Node? Previous { get; set; }
+
+        public Node? Next { get; set; }
+    }
+
+    private Node? _head;
+    private Node? _tail;
+
+    public int Length { get; private set; }
+
+    public void Add(T e) => AddAt(Length, e);
+
+    public void AddAt(int index, T e)
+    {
+        ValidateInsertIndex(index);
+
+        Node newNode = new(e);
+
+        if (Length == 0)
         {
-            throw new NotImplementedException();
+            _head = _tail = newNode;
+        }
+        else if (index == 0)
+        {
+            newNode.Next = _head;
+            _head!.Previous = newNode;
+            _head = newNode;
+        }
+        else if (index == Length)
+        {
+            newNode.Previous = _tail;
+            _tail!.Next = newNode;
+            _tail = newNode;
+        }
+        else
+        {
+            Node current = GetNodeAt(index);
+            Node previous = current.Previous!;
+
+            newNode.Previous = previous;
+            newNode.Next = current;
+            previous.Next = newNode;
+            current.Previous = newNode;
         }
 
-        public void AddAt(int index, T e)
+        Length++;
+    }
+
+    public T ElementAt(int index) => GetNodeAt(index).Value;
+
+    public IEnumerator<T> GetEnumerator() => new Enumerator(_head);
+
+    public void Remove(T item)
+    {
+        Node? current = _head;
+
+        while (current is not null)
         {
-            throw new NotImplementedException();
+            if (EqualityComparer<T>.Default.Equals(current.Value, item))
+            {
+                RemoveNode(current);
+                return;
+            }
+
+            current = current.Next;
+        }
+    }
+
+    public T RemoveAt(int index)
+    {
+        Node node = GetNodeAt(index);
+        T value = node.Value;
+
+        RemoveNode(node);
+
+        return value;
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+    private static void ValidateInsertIndex(int index)
+    {
+        if (index < 0)
+        {
+            throw new IndexOutOfRangeException();
+        }
+    }
+
+    private Node GetNodeAt(int index)
+    {
+        if (index < 0 || index >= Length)
+        {
+            throw new IndexOutOfRangeException();
         }
 
-        public T ElementAt(int index)
+        if (index <= Length / 2)
         {
-            throw new NotImplementedException();
+            Node current = _head!;
+
+            for (int i = 0; i < index; i++)
+            {
+                current = current.Next!;
+            }
+
+            return current;
         }
 
-        public IEnumerator<T> GetEnumerator()
+        Node tail = _tail!;
+
+        for (int i = Length - 1; i > index; i--)
         {
-            throw new NotImplementedException();
+            tail = tail.Previous!;
         }
 
-        public void Remove(T item)
+        return tail;
+    }
+
+    private void RemoveNode(Node node)
+    {
+        Node? previous = node.Previous;
+        Node? next = node.Next;
+
+        if (previous is null)
         {
-            throw new NotImplementedException();
+            _head = next;
+        }
+        else
+        {
+            previous.Next = next;
         }
 
-        public T RemoveAt(int index)
+        if (next is null)
         {
-            throw new NotImplementedException();
+            _tail = previous;
+        }
+        else
+        {
+            next.Previous = previous;
         }
 
-        IEnumerator IEnumerable.GetEnumerator()
+        Length--;
+    }
+
+    private sealed class Enumerator(Node? head) : IEnumerator<T>
+    {
+        private Node? _current;
+        private bool _started;
+
+        public T Current => _current!.Value;
+
+        object IEnumerator.Current => _current!.Value!;
+
+        public bool MoveNext()
         {
-            throw new NotImplementedException();
+            if (!_started)
+            {
+                _current = head;
+                _started = true;
+            }
+            else
+            {
+                _current = _current?.Next;
+            }
+
+            return _current is not null;
         }
+
+        public void Reset()
+        {
+            _started = false;
+            _current = null;
+        }
+
+        public void Dispose() { }
     }
 }
--- Module6_DataStructures/Tasks/HybridFlowProcessor.cs
+++ Module6_DataStructures/Tasks/HybridFlowProcessor.cs
@@ -1,28 +1,33 @@
 ﻿using System;
 using Tasks.DoNotChange;
 
-namespace Tasks
+namespace Tasks;
+
+public class HybridFlowProcessor<T> : IHybridFlowProcessor<T>
 {
-    public class HybridFlowProcessor<T> : IHybridFlowProcessor<T>
+    private readonly DoublyLinkedList<T> _storage = [];
+
+    public T Dequeue()
     {
-        public T Dequeue()
-        {
-            throw new NotImplementedException();
-        }
+        EnsureNotEmpty();
+        return _storage.RemoveAt(0);
+    }
 
-        public void Enqueue(T item)
-        {
-            throw new NotImplementedException();
-        }
+    public void Enqueue(T item) => _storage.Add(item);
 
-        public T Pop()
-        {
-            throw new NotImplementedException();
-        }
+    public T Pop()
+    {
+        EnsureNotEmpty();
+        return _storage.RemoveAt(_storage.Length - 1);
+    }
 
-        public void Push(T item)
+    public void Push(T item) => _storage.Add(item);
+
+    private void EnsureNotEmpty()
+    {
+        if (_storage.Length == 0)
         {
-            throw new NotImplementedException();
+            throw new InvalidOperationException();
         }
     }
 }
