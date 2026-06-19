0cad62b818165cd39d39c894e71f888e2a344983
asoliev <91322310+asoliev@users.noreply.github.com>
2026-06-19 19:12:13 +0500

    refactor: simplify test code and improve readability in DoublyLinkedListTests and HybridFlowProcessorTest

--- Module6_DataStructures/Tasks.Tests/HybridFlowProcessorTest.cs
+++ Module6_DataStructures/Tasks.Tests/HybridFlowProcessorTest.cs
@@ -2,70 +2,69 @@
 using FluentAssertions;
 using Xunit;
 
-namespace Tasks.Tests
+namespace Tasks.Tests;
+
+public class HybridFlowProcessorTest
 {
-    public class HybridFlowProcessorTest
+    [Fact]
+    public void Should_Pop_Item_From_The_Top_Of_Processing_Items()
     {
-        [Fact]
-        public void Should_Pop_Item_From_The_Top_Of_Processing_Items()
-        {
-            var processor = new HybridFlowProcessor<int>();
-            processor.Push(5);
-            processor.Push(6);
+        HybridFlowProcessor<int> processor = new();
+        processor.Push(5);
+        processor.Push(6);
 
-            var actualElement = processor.Pop();
+        int actualElement = processor.Pop();
 
-            actualElement.Should().Be(6);
-        }
+        actualElement.Should().Be(6);
+    }
 
-        [Fact]
-        public void Should_Throw_InvalidOperationExceptiong_If_Processor_Queue_Is_Empty_And_Pop_Called()
-        {
-            var processor = new HybridFlowProcessor<int>();
+    [Fact]
+    public void Should_Throw_InvalidOperationExceptiong_If_Processor_Queue_Is_Empty_And_Pop_Called()
+    {
+        HybridFlowProcessor<int> processor = new();
 
-            Action action = () => processor.Pop();
+        Action action = () => processor.Pop();
 
-            action.Should().Throw<InvalidOperationException>();
-        }
+        action.Should().Throw<InvalidOperationException>();
+    }
 
-        [Fact]
-        public void Should_Dequeue_Item_From_The_Beginning_Of_Processing_Items()
-        {
-            var processor = new HybridFlowProcessor<int>();
-            processor.Enqueue(5);
-            processor.Enqueue(6);
+    [Fact]
+    public void Should_Dequeue_Item_From_The_Beginning_Of_Processing_Items()
+    {
+        HybridFlowProcessor<int> processor = new();
+        processor.Enqueue(5);
+        processor.Enqueue(6);
 
-            processor.Dequeue().Should().Be(5);
-        }
+        processor.Dequeue().Should().Be(5);
+    }
 
-        [Fact]
-        public void Should_Throw_InvalidOperationExceptiong_If_Processor_Queue_Is_Empty_And_Dequeue_Called()
-        {
-            var processor = new HybridFlowProcessor<int>();
+    [Fact]
+    public void Should_Throw_InvalidOperationExceptiong_If_Processor_Queue_Is_Empty_And_Dequeue_Called()
+    {
+        HybridFlowProcessor<int> processor = new();
 
-            Action action = () => processor.Dequeue();
+        Action action = () => processor.Dequeue();
 
-            action.Should().Throw<InvalidOperationException>();
-        }
+        action.Should().Throw<InvalidOperationException>();
+    }
 
-        [Fact]
-        public void Should_Dequeue_Item_From_The_Beginning_Of_Processing_Items_With_Mix_Add()
-        {
-            var processor = new HybridFlowProcessor<int>();
-            processor.Push(5);
-            processor.Enqueue(6);
+    [Fact]
+    public void Should_Dequeue_Item_From_The_Beginning_Of_Processing_Items_With_Mix_Add()
+    {
+        HybridFlowProcessor<int> processor = new();
+        processor.Push(5);
+        processor.Enqueue(6);
 
-            processor.Dequeue().Should().Be(5);
-        }
+        processor.Dequeue().Should().Be(5);
+    }
 
-        [Fact]
-        public void Should_Pop_Item_From_The_Beginning_Of_Processing_Items_With_Mix_Add()
-        {
-            var processor = new HybridFlowProcessor<int>();
-            processor.Enqueue(6);
-            processor.Push(5);
+    [Fact]
+    public void Should_Pop_Item_From_The_Beginning_Of_Processing_Items_With_Mix_Add()
+    {
+        HybridFlowProcessor<int> processor = new();
+        processor.Enqueue(6);
+        processor.Push(5);
 
-            processor.Pop().Should().Be(5);
-        }
+        processor.Pop().Should().Be(5);
     }
 }
