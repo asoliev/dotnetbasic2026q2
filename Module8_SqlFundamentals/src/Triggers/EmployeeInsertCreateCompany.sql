77bb38f56b5aedb3a2a6ada3b8f82e8871cd260b
asoliev <91322310+asoliev@users.noreply.github.com>
2026-06-21 21:30:19 +0500

    feat(task4): refactor TR_Employee_Insert_CreateCompany trigger to use set-based operations for address and company creation

--- Module8_SqlFundamentals/src/Triggers/EmployeeInsertCreateCompany.sql
+++ Module8_SqlFundamentals/src/Triggers/EmployeeInsertCreateCompany.sql
@@ -1,55 +1,54 @@
 CREATE TRIGGER [dbo].[TR_Employee_Insert_CreateCompany]
-ON [dbo].[Employee]
-AFTER INSERT
+    ON [dbo].[Employee]
+    AFTER INSERT
 AS
 BEGIN
     SET NOCOUNT ON;
 
-    DECLARE @CompanyName NVARCHAR(20);
-    DECLARE @Street NVARCHAR(50);
-    DECLARE @City NVARCHAR(20);
-    DECLARE @State NVARCHAR(50);
-    DECLARE @ZipCode NVARCHAR(50);
-    DECLARE @NewAddressId INT;
+    -- Distinct new company+address combos that don't already exist
+    DECLARE @ToCreate TABLE
+    (
+        RowKey      INT IDENTITY(1,1) PRIMARY KEY,
+        CompanyName NVARCHAR(20),
+        Street      NVARCHAR(50),
+        City        NVARCHAR(20),
+        State       NVARCHAR(50),
+        ZipCode     NVARCHAR(50)
+    );
 
-    DECLARE company_cursor CURSOR LOCAL FAST_FORWARD FOR
-        SELECT DISTINCT
-            LEFT(i.[CompanyName], 20) AS [CompanyName],
-            a.[Street],
-            a.[City],
-            a.[State],
-            a.[ZipCode]
-        FROM inserted AS i
+    INSERT INTO @ToCreate (CompanyName, Street, City, State, ZipCode)
+    SELECT DISTINCT
+        LEFT(i.[CompanyName], 20),
+        a.[Street], a.[City], a.[State], a.[ZipCode]
+    FROM inserted AS i
         INNER JOIN [dbo].[Address] AS a ON a.[Id] = i.[AddressId]
-        WHERE NOT EXISTS
-        (
-            SELECT 1
-            FROM [dbo].[Company] AS c
-            INNER JOIN [dbo].[Address] AS ca ON ca.[Id] = c.[AddressId]
-            WHERE c.[Name] = LEFT(i.[CompanyName], 20)
-              AND ISNULL(ca.[Street], '') = ISNULL(a.[Street], '')
-              AND ISNULL(ca.[City], '') = ISNULL(a.[City], '')
-              AND ISNULL(ca.[State], '') = ISNULL(a.[State], '')
-              AND ISNULL(ca.[ZipCode], '') = ISNULL(a.[ZipCode], '')
-        );
+    WHERE NOT EXISTS
+    (
+        SELECT 1
+        FROM [dbo].[Company]  AS c
+        INNER JOIN [dbo].[Address] AS ca ON ca.[Id] = c.[AddressId]
+        WHERE c.[Name] = LEFT(i.[CompanyName], 20)
+          AND ISNULL(ca.[Street], '')  = ISNULL(a.[Street], '')
+          AND ISNULL(ca.[City], '')    = ISNULL(a.[City], '')
+          AND ISNULL(ca.[State], '')   = ISNULL(a.[State], '')
+          AND ISNULL(ca.[ZipCode], '') = ISNULL(a.[ZipCode], '')
+    );
 
-    OPEN company_cursor;
+    -- Set-based address copy with old->new mapping
+    DECLARE @AddressMap TABLE (RowKey INT, NewAddressId INT);
 
-    FETCH NEXT FROM company_cursor INTO @CompanyName, @Street, @City, @State, @ZipCode;
+    MERGE INTO [dbo].[Address] AS tgt
+    USING @ToCreate AS src
+    ON 1 = 0
+    WHEN NOT MATCHED THEN
+        INSERT ([Street], [City], [State], [ZipCode])
+        VALUES (src.[Street], src.[City], src.[State], src.[ZipCode])
+    OUTPUT src.RowKey, inserted.[Id]
+    INTO @AddressMap (RowKey, NewAddressId);
 
-    WHILE @@FETCH_STATUS = 0
-    BEGIN
-        INSERT INTO [dbo].[Address] ([Street], [City], [State], [ZipCode])
-        VALUES (@Street, @City, @State, @ZipCode);
-
-        SET @NewAddressId = CAST(SCOPE_IDENTITY() AS INT);
-
-        INSERT INTO [dbo].[Company] ([Name], [AddressId])
-        VALUES (@CompanyName, @NewAddressId);
-
-        FETCH NEXT FROM company_cursor INTO @CompanyName, @Street, @City, @State, @ZipCode;
-    END;
-
-    CLOSE company_cursor;
-    DEALLOCATE company_cursor;
+    -- Create companies attached to the new addresses
+    INSERT INTO [dbo].[Company] ([Name], [AddressId])
+    SELECT t.CompanyName, m.NewAddressId
+    FROM @ToCreate AS t
+        INNER JOIN @AddressMap AS m ON m.RowKey = t.RowKey;
 END;
