9de0015507b2137b3970c46f1548113397710572
asoliev <91322310+asoliev@users.noreply.github.com>
2026-06-21 20:57:19 +0500

    refactor(task1): update SQL table definitions for Address, Company, Employee, and Person to improve structure and readability

--- Module8_SqlFundamentals/src/Tables/Company.sql
+++ Module8_SqlFundamentals/src/Tables/Company.sql
@@ -1,7 +1,13 @@
 CREATE TABLE [dbo].[Company]
 (
-    [Id] INT IDENTITY(1,1) NOT NULL CONSTRAINT [PK_Company] PRIMARY KEY,
-    [Name] NVARCHAR(20) NOT NULL,
-    [AddressId] INT NOT NULL,
-    CONSTRAINT [FK_Company_Address] FOREIGN KEY ([AddressId]) REFERENCES [dbo].[Address]([Id])
+    [Id]        INT          IDENTITY (1, 1) NOT NULL,
+    [Name]      NVARCHAR(20) NOT NULL,
+    [AddressId] INT          NOT NULL,
+    CONSTRAINT [PK_Company] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Company_Address] FOREIGN KEY ([AddressId])
+        REFERENCES [dbo].[Address] ([Id])
 );
+
+GO
+CREATE NONCLUSTERED INDEX [IX_Company_AddressId]
+    ON [dbo].[Company] ([AddressId]);
--- Module8_SqlFundamentals/src/Tables/Employee.sql
+++ Module8_SqlFundamentals/src/Tables/Employee.sql
@@ -1,11 +1,22 @@
 CREATE TABLE [dbo].[Employee]
 (
-    [Id] INT IDENTITY(1,1) NOT NULL CONSTRAINT [PK_Employee] PRIMARY KEY,
-    [AddressId] INT NOT NULL,
-    [PersonId] INT NOT NULL,
-    [CompanyName] NVARCHAR(20) NOT NULL,
-    [Position] NVARCHAR(30) NULL,
+    [Id]           INT           IDENTITY (1, 1) NOT NULL,
+    [AddressId]    INT           NOT NULL,
+    [PersonId]     INT           NOT NULL,
+    [CompanyName]  NVARCHAR(20)  NOT NULL,
+    [Position]     NVARCHAR(30)  NULL,
     [EmployeeName] NVARCHAR(100) NULL,
-    CONSTRAINT [FK_Employee_Address] FOREIGN KEY ([AddressId]) REFERENCES [dbo].[Address]([Id]),
-    CONSTRAINT [FK_Employee_Person] FOREIGN KEY ([PersonId]) REFERENCES [dbo].[Person]([Id])
+    CONSTRAINT [PK_Employee] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Employee_Address] FOREIGN KEY ([AddressId])
+        REFERENCES [dbo].[Address] ([Id]),
+    CONSTRAINT [FK_Employee_Person] FOREIGN KEY ([PersonId])
+        REFERENCES [dbo].[Person] ([Id])
 );
+
+GO
+CREATE NONCLUSTERED INDEX [IX_Employee_AddressId]
+    ON [dbo].[Employee] ([AddressId]);
+
+GO
+CREATE NONCLUSTERED INDEX [IX_Employee_PersonId]
+    ON [dbo].[Employee] ([PersonId]);
