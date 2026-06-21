f99abb4b13558baf3376836a927d12a7a93f37a9
asoliev <91322310+asoliev@users.noreply.github.com>
2026-06-21 20:58:24 +0500

    refactor(task2): enhance readability of EmployeeInfo SQL view by adding comments and formatting

--- Module8_SqlFundamentals/src/Views/EmployeeInfo.sql
+++ Module8_SqlFundamentals/src/Views/EmployeeInfo.sql
@@ -1,11 +1,28 @@
 CREATE VIEW [dbo].[EmployeeInfo]
 AS
-SELECT TOP (100) PERCENT
-    e.[Id] AS [EmployeeId],
-    COALESCE(NULLIF(LTRIM(RTRIM(e.[EmployeeName])), ''), CONCAT(p.[FirstName], ' ', p.[LastName])) AS [EmployeeFullName],
-    CONCAT(COALESCE(a.[ZipCode], ''), '_', COALESCE(a.[State], ''), ', ', COALESCE(a.[City], ''), '-', a.[Street]) AS [EmployeeFullAddress],
-    CONCAT(e.[CompanyName], '(', COALESCE(e.[Position], ''), ')') AS [EmployeeCompanyInfo]
-FROM [dbo].[Employee] AS e
-INNER JOIN [dbo].[Person] AS p ON p.[Id] = e.[PersonId]
-INNER JOIN [dbo].[Address] AS a ON a.[Id] = e.[AddressId]
-ORDER BY e.[CompanyName] ASC, a.[City] ASC;
+    SELECT TOP (100) PERCENT
+        e.[Id] AS [EmployeeId],
+
+        -- EmployeeName if not null/empty/whitespace, otherwise '{FirstName} {LastName}'
+        COALESCE(
+            NULLIF(LTRIM(RTRIM(e.[EmployeeName])), ''),
+            CONCAT(p.[FirstName], ' ', p.[LastName])
+        ) AS [EmployeeFullName],
+
+        -- '{ZipCode}_{State}, {City}-{Street}'  (NULL-safe)
+        CONCAT(
+            COALESCE(a.[ZipCode], ''), '_',
+            COALESCE(a.[State],   ''), ', ',
+            COALESCE(a.[City],    ''), '-',
+            a.[Street]
+        ) AS [EmployeeFullAddress],
+
+        -- '{CompanyName}({Position})'  (NULL-safe)
+        CONCAT(e.[CompanyName], '(', COALESCE(e.[Position], ''), ')') AS [EmployeeCompanyInfo]
+
+    FROM [dbo].[Employee] AS e
+        INNER JOIN [dbo].[Person]  AS p ON p.[Id] = e.[PersonId]
+        INNER JOIN [dbo].[Address] AS a ON a.[Id] = e.[AddressId]
+    ORDER BY
+        e.[CompanyName] ASC,
+        a.[City] ASC;
