4d8c833a0b56f4de00d4ec14f2f5e284317cf3b4
asoliev <91322310+asoliev@users.noreply.github.com>
2026-06-26 19:10:35 +0500

    refactor: simplify FizzBuzz tests with theory

--- Module7_UnitTesting/list/FizzBuzz_12/FizzBuzzTests.cs
+++ Module7_UnitTesting/list/FizzBuzz_12/FizzBuzzTests.cs
@@ -5,35 +5,15 @@ namespace FizzBuzz_12;
 
 public class FizzBuzzTests
 {
-    [Fact]
-    public void GetValue_Should_Return_Number_As_String()
+    [Theory]
+    [InlineData(1, "1")]
+    [InlineData(3, "Fizz")]
+    [InlineData(5, "Buzz")]
+    [InlineData(15, "FizzBuzz")]
+    public void GetValue_Should_Return_Expected_Result(int number, string expected)
     {
-        string actual = FizzBuzz.GetValue(1);
+        string actual = FizzBuzz.GetValue(number);
 
-        actual.Should().Be("1");
-    }
-
-    [Fact]
-    public void GetValue_Should_Return_Fizz_For_Multiples_Of_Three()
-    {
-        string actual = FizzBuzz.GetValue(3);
-
-        actual.Should().Be("Fizz");
-    }
-
-    [Fact]
-    public void GetValue_Should_Return_Buzz_For_Multiples_Of_Five()
-    {
-        string actual = FizzBuzz.GetValue(5);
-
-        actual.Should().Be("Buzz");
-    }
-
-    [Fact]
-    public void GetValue_Should_Return_FizzBuzz_For_Multiples_Of_Three_And_Five()
-    {
-        string actual = FizzBuzz.GetValue(15);
-
-        actual.Should().Be("FizzBuzz");
+        actual.Should().Be(expected);
     }
 }
