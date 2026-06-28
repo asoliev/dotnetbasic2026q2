e3c90dcda272b29ba0fc7da38b018347fb0e6b86
asoliev <91322310+asoliev@users.noreply.github.com>
2026-06-28 11:51:49 +0500

    Refactor Program.cs to consolidate application setup; remove Startup.cs for streamlined configuration

--- Module9_Logging/Logging/BrainstormSessions/Program.cs
+++ Module9_Logging/Logging/BrainstormSessions/Program.cs
@@ -1,16 +1,67 @@
-using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using BrainstormSessions.Core.Interfaces;
+using BrainstormSessions.Core.Model;
+using BrainstormSessions.Infrastructure;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace BrainstormSessions;
+var builder = WebApplication.CreateBuilder(args);
 
-public class Program
+builder.Services.AddDbContext<AppDbContext>(
+    optionsBuilder => optionsBuilder.UseInMemoryDatabase("InMemoryDb"));
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddScoped<IBrainstormSessionRepository, EFStormSessionRepository>();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
 {
-    public static void Main(string[] args) => CreateHostBuilder(args).Build().Run();
+    using IServiceScope scope = app.Services.CreateScope();
+    IBrainstormSessionRepository repository = scope.ServiceProvider.GetRequiredService<IBrainstormSessionRepository>();
 
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.UseStartup<Startup>();
-            });
+    await SeedDatabaseAsync(repository);
+}
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+
+static async Task SeedDatabaseAsync(IBrainstormSessionRepository repo)
+{
+    List<BrainstormSession> sessionList = await repo.ListAsync();
+    if (sessionList.Count == 0)
+    {
+        await repo.AddAsync(GetTestSession());
+    }
+}
+
+static BrainstormSession GetTestSession()
+{
+    var session = new BrainstormSession()
+    {
+        Name = "Test Session 1",
+        DateCreated = new DateTime(2016, 8, 1)
+    };
+
+    var idea = new Idea()
+    {
+        DateCreated = new DateTime(2016, 8, 1),
+        Description = "Totally awesome idea",
+        Name = "Awesome idea"
+    };
+
+    session.AddIdea(idea);
+    return session;
 }
